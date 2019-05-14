5c65417acd6b17c6e1fa49312baba881995ea6f2
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-14 17:04:24 +0200

    tried to work on style...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "faker"
+# require "faker"
 
 # TODO: Write a seed to insert 100 posts in the database
-100.times do
-  article = Article.new(title: Faker::Ancient.primordial, content: Faker::ChuckNorris.fact)
-  article.save
-end
+# 100.times do
+#   article = Article.new(title: Faker::Ancient.primordial, content: Faker::ChuckNorris.fact)
+#   article.save
+# end
+articles = Article.create([{title: 'Trump Urges Americans to Boycott Chinese Goods and Just Buy Things at Walmart', content: "WASHINGTON (The Borowitz Report)—Asking for their solidarity in his trade war with China, Donald Trump is urging Americans to boycott Chinese goods and “just buy things at Walmart.”
+
+Trump made his request via Twitter, where he told his fellow-citizens that it was their “patriotic duty” to punish China by buying as many goods at Walmart as possible.
+
+“If you go to a GREAT AMERICAN STORE like Walmart, you’ll find lots of cheap sportswear, shoes, and other items for you and your family to enjoy,” he tweeted. “What better way to show China that we don’t need their DUMB STUFF!”
+
+Shortly after Trump sent those marching orders to his countrymen, the Chinese President, Xi Jinping, offered a muted response. “I’m beginning to see how he lost a billion dollars,” Xi said."},{ title: 'Americans Beg Trump for Secret to Staying So Young and Vibrant', content: "WASHINGTON (The Borowitz Report)—Americans across the country wish that Donald Trump would reveal his secret to staying so young and vibrant, the nation’s personal trainers have confirmed.
+
+According to Tracy Klugian, a fitness professional in St. Louis, “not a day goes by” without one of his clients requesting a physical regimen that will give him or her the youthful appearance and mental sharpness that have made Trump the envy of millions.
+
+“Clients will come in and say, ‘Make me as young and vibrant as Donald Trump,’ ” Klugian said. “I have to warn them that that’s setting the bar very, very high.”
+
+Having analyzed Trump’s fitness habits, Klugian has theorized that his startling youthfulness and off-the-charts vibrance might be the result of eight hours that he devotes each day to “executive time.”
+
+“Lifting the remote, putting it down, lifting it again, and then tweeting is an exercise routine that very few people could keep up for one hour, and Trump does it for eight,” he said. “I tell my clients, if you want to be as young and vibrant as Donald Trump, you’re going to have to put in that kind of time.”
+
+Klugian said that he was also putting clients on a “Trump diet,” consisting entirely of hamburgers and Coke, to give their bodies the fuel necessary to complete the gruelling remote-lifting-and-tweeting workout.
+
+But the personal trainer said that he urges his clients not to be discouraged if, even after adopting Trump’s rigorous exercise routine and diet, they fail to achieve Trump’s physical perfection.
+
+“At the end of the day, Donald Trump has found the Fountain of Youth, and also the Fountain of Vibrance,” he said."}, { title: 'Americans Fear That Former Trump Staffers Will Be Released Into Their Cities', content: "WASHINGTON (The Borowitz Report)—Americans are in a state of abject panic amid reports that Donald J. Trump is threatening to dump thousands of fired Cabinet members and aides on cities that do not support him.
+
+Harland Dorrinson, who lives in San Francisco, said that “every American should be alarmed” at the spectre of former Trump staffers like Kirstjen Nielsen, Scott Pruitt, and Anthony Scaramucci descending on their towns.
+
+“A lot of these people were unsavory to begin with, and their time at the White House only hardened them,” he said.
+
+Carol Foyler, who resides in Boston, said that, with Trump staffers being fired at a rate of four hundred a day, she lives in terror at the prospect of these castoffs melting into the general population.
+
+“I was on line at Starbucks the other day and I thought I saw Steve Bannon,” she said. “It turned out it was just some other creepy-looking guy, but my heart was racing. The fear is real.”
+
+Tracy Klugian, who lives in Minneapolis, has started a petition to create a city ordinance preventing former Trump aides from settling in his town. “This city is full,” he said.
+
+As they brace themselves for an onslaught of fired Trump underlings, some Americans are grasping for a silver lining. “As of now, Stephen Miller and Sarah Huckabee Sanders are still employed at the White House, where their movements can be closely monitored,” Foyler said. “But, it if that changes, God help us all.”"}, { title: "The Stock Market Intrudes on the Alternate Reality of Trump’s Trade War", content: "As the news emerged, last week, that trade talks between the United States and China had broken down, and the Trump Administration was moving to raise tariffs on hundreds of billions of dollars’ worth of imports from China, Donald Trump took to Twitter to promote an alternative reality. He argued that higher import duties don’t represent an additional cost to consumers and a drag on over-all economic activity, as the economics textbooks say, but are, in fact, good for the economy.
+
+“I am very happy with over $100 Billion a year in Tariffs filling U.S. coffers...great for U.S., not good for China!” Trump tweeted last Wednesday. On Friday, he reiterated the message, saying, “Tariffs will make our Country MUCH STRONGER, not weaker. Just sit back and watch!” And this Monday morning, he added, “The unexpectedly good first quarter 3.2% GDP was greatly helped by Tariffs from China. Some people just don’t get it!”
+
+He’s right about that. Nearly all economists, including some who work for him, believe that tariffs are costly to the economy, and that American consumers will bear at least some of the burden as the Trump Administration raises the tariff rate from ten per cent to twenty-five per cent on thousands of imported Chinese products, including glassware, motorboats, vacuum cleaners, and metal doors and windows. When Larry Kudlow, Trump’s top economic adviser, appeared on Fox News on Sunday, the host Chris Wallace pointed out that tariffs are, in effect, taxes that are often passed on to consumers as higher prices. “Fair enough,” Kudlow replied. “In fact, both sides will pay. Both sides will pay in these things.” Evidently, Kudlow didn’t explain this to his boss, or Trump didn’t listen.
+
+Kudlow also argued that it is worth bearing the short-term pain of a trade war to force China to open up its vast economy to American firms on a fairer basis. Conceivably, Trump could make this case, too—and if he did, he would have a good deal of support from U.S. business leaders and even some Democratic officials. But, instead, he has promoted the fiction that trade wars are painless and easy to win. In his Monday morning tweets, he insisted, “There is no reason for the U.S. Consumer to pay the Tariffs, which take effect on China today.” He went on to suggest that Americans could “by (sic) from a non-Tariffed country” or “buy the product from inside the USA (best idea.) That’s Zero Tariffs.”
+
+Doubtless, Trump will be taking his own advice. He could begin by trying to buy himself a shiny new iPhone XS, on which to bash out some more pearls of economic wisdom. Unfortunately, he won’t be able to find one that isn’t made in China. If he goes ahead with the purchase anyway, and his Administration follows through on its threat to widen its tariffs to all Chinese imports, not just the ones currently targeted, he could end up paying an extra hundred and sixty dollars for the new phone, which currently costs about a thousand dollars, according to Morgan Stanley. Apple assembles practically all of its iPhones, iPads, and other products in mainland China, as do many other U.S. companies. Even if these firms took Trump’s advice and tried to shift production to other countries, or to the United States, it would take them months or years to do it.
+
+ma, of course. After he settles on a pet theory, he’s largely oblivious to reason, and can only be shifted by brute force—the sort of force that can only be applied by sharply falling approval ratings, or, even more potently, by sharply falling stock prices. You only have to glance at Trump’s Twitter feed to know that he is obsessed with the stock market and believes it is a key to his political appeal. Here’s a somewhat random example from last Friday: “The average 401(k) balance has SOARED since the bottom of the market - 466%. Wow!”
+
+He shouldn’t have tempted fate. On Monday morning, shortly after he had said that China shouldn’t retaliate against his Administration’s latest moves, the Chinese government announced it would do just that. The tariffs it imposes on a range of U.S.-made goods—including vegetables, beer, and liquified natural gas—would be raised significantly, the Chinese foreign ministry said. If the timing of this announcement, which came just before the opening bell on Wall Street, was intended to spook American investors, it worked. As soon as trading started, the Dow plunged almost five hundred points from the previous close and fell further from there. At the end of the trading session, it was down 618 points—or about 2.4 per cent—at 25,325.
+
+By itself, this wasn’t an earthshaking development. Since the start of 2019, the Dow has risen sharply, and even after Monday’s fall it is up by about eight and a half per cent. But it has also fallen about four and a half per cent in a week. That’s a significant move, and it indicates that investors are reappraising their optimistic view of the future in light of Trump’s revving up of his trade war. If both sides now dig in—no further negotiations have been scheduled—the sentiment in the market could turn even more sharply negative, producing more days like Monday."}])
